9c602c6a30ee67ab895a2668426007e3d464e0f8
hugobepa <hugo_bcn@hotmail.com>
2025-12-20 22:15:03 +0100

    appComp_ThemeProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,27 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { Slot } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, useColorScheme, View } from "react-native";
 import "../global.css";
 
 export default function RootLayout() {
   //const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, "background");
+  const colorScheme = useColorScheme();
 
   return (
-    <View className="flex flex-1 items-center justify-center bg-light-background dark:bg-dark-background">
-      <Text className="mt-20 text-xl font-bold text-light-primary">
-        Welcome to Layout!
-      </Text>
-      <Slot />
-    </View>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <View className="flex flex-1 items-center justify-center bg-light-background dark:bg-dark-background">
+        <Text className="mt-20 text-xl font-bold text-light-primary">
+          Welcome to Layout!
+        </Text>
+        <Slot />
+      </View>
+    </ThemeProvider>
   );
 }
