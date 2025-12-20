b73a005a46a0a3de6e4bc09a245d8cb0bce179f1
hugobepa <hugo_bcn@hotmail.com>
2025-12-20 21:59:50 +0100

    comit_inicial_instalacionNativeWind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Slot } from "expo-router";
+import { Text, View } from "react-native";
+import "../global.css";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  //const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View className="flex flex-1 items-center justify-center bg-light-background dark:bg-dark-background">
+      <Text className="mt-20 text-xl font-bold text-light-primary">
+        Welcome to Layout!
+      </Text>
+      <Slot />
+    </View>
   );
 }
