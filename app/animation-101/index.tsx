85202c39edec57e2abc9ede8f33da29412c78871
hugobepa <hugo_bcn@hotmail.com>
2025-12-21 20:00:11 +0100

    CompApp_ThemedButton

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native';
+import ThemedButton from "@/presentation/shared/ThemedButton";
+import ThemedView from "@/presentation/shared/ThemedView";
 
 const Animation101Screen = () => {
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedButton className="mb-5" onPress={() => console.log("fadeIn")}>
+        fadeIn
+      </ThemedButton>
+      <ThemedButton className="mb-5" onPress={() => console.log("fadeOut")}>
+        fadeOut
+      </ThemedButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
