fbf5e96853c99b2ded222789d9c8730a19f58be9
hugobepa <hugo_bcn@hotmail.com>
2025-12-21 13:54:24 +0100

    appComp_ThemedView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,23 +5,32 @@ import {
 } from "@react-navigation/native";
 
 import { useThemeColor } from "@/hooks/use-theme-color";
+import ThemedView from "@/presentation/shared/ThemedView";
 import { Slot } from "expo-router";
-import { Text, useColorScheme, View } from "react-native";
+import { Text, useColorScheme } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "../global.css";
 
 export default function RootLayout() {
   //const colorScheme = useColorScheme();
-  const backgroundColor = useThemeColor({}, "background");
+  const backgroundColor = useThemeColor(
+    { light: "red", dark: "indigo" },
+    "background"
+  );
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <View className="flex flex-1 items-center justify-center bg-light-background dark:bg-dark-background">
-        <Text className="mt-20 text-xl font-bold text-light-primary">
-          Welcome to Layout!
-        </Text>
-        <Slot />
-      </View>
-    </ThemeProvider>
+    <GestureHandlerRootView
+      style={{ backgroundColor: backgroundColor, flex: 1 }}
+    >
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <ThemedView margin>
+          <Text className="mt-20 text-xl font-bold text-light-primary">
+            Welcome to Layout!
+          </Text>
+          <Slot />
+        </ThemedView>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 const index = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Welcome to Index!</Text>
+    <View>
+      <Text>Welcome to Index!</Text>
     </View>
   );
 };
